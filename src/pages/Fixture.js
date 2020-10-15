a10b6aeba205f9b826ff6559e8ac2625dd2c129b
Paramet Kongjaroen <parametprame2@gmail.com>
2020-10-15 21:06:41 +0700

    Update Fixture.js

--- src/pages/Fixture.js
+++ src/pages/Fixture.js
@@ -1,56 +1,219 @@
 import React, { useState, useEffect } from "react";
 import _ from "lodash";
+import { trackPromise } from "react-promise-tracker";
+import axios from "axios";
 
-class MatchData {
-  constructor(id, stadiumNumber, kickOffTime, homeTeam, awayTeam, refereeTeam) {
-    this.id = id;
-    this.stadiumNumber = stadiumNumber;
-    this.kickOffTime = kickOffTime;
-    this.homeTeam = homeTeam;
-    this.homeScores = 0;
-    this.awayTeam = awayTeam;
-    this.awayScores = 0;
-    this.refereeTeam = refereeTeam;
-  }
-}
+// class MatchData {
+//   constructor(id, stadiumNumber, kickOffTime, homeTeam, awayTeam, refereeTeam) {
+//     this.id = id;
+//     this.stadiumNumber = stadiumNumber;
+//     this.kickOffTime = kickOffTime;
+//     this.homeTeam = homeTeam;
+//     this.homeScores = 0;
+//     this.awayTeam = awayTeam;
+//     this.awayScores = 0;
+//     this.refereeTeam = refereeTeam;
+//   }
+// }
 
-let matches = [];
-for (let i = 1; i <= 36; i++) {
-  matches.push(
-    new MatchData(
-      i,
-      Math.floor(Math.random() * 2) + 1,
-      new Date().toLocaleTimeString("th-TH", {
-        hour12: false,
-        hour: "2-digit",
-        minute: "2-digit",
-      }),
-      `รุ่น ${Math.floor(Math.random() * 9)}`,
-      `รุ่น ${Math.floor(Math.random() * 9)}`,
-      `รุ่น ${Math.floor(Math.random() * 9)}`,
-      Math.floor(Math.random() * 9)
-    )
-  );
-}
+// let matches = [];
+// for (let i = 1; i <= 36; i++) {
+//   matches.push(
+//     new MatchData(
+//       i,
+//       Math.floor(Math.random() * 2) + 1,
+//       new Date().toLocaleTimeString("th-TH", {
+//         hour12: false,
+//         hour: "2-digit",
+//         minute: "2-digit",
+//       }),
+//       `รุ่น ${Math.floor(Math.random() * 9)}`,
+//       `รุ่น ${Math.floor(Math.random() * 9)}`,
+//       `รุ่น ${Math.floor(Math.random() * 9)}`,
+//       Math.floor(Math.random() * 9)
+//     )
+//   );
+// }
+
+// const Fixture = () => {
+//   _.sortBy(matches, ["kickOffTime", "stadiumNumber"]);
+//   const [fixture, setFixture] = useState(matches);
+//   const [fixtureFiltered, setFixtureFiltered] = useState(fixture);
+//   const [stadium, setStadium] = useState(0);
+
+//   useEffect(() => {
+//     if (+stadium === 0) {
+//       setFixtureFiltered(fixture);
+//     } else {
+//       let filtered = fixture.filter(
+//         (match) => match.stadiumNumber === +stadium
+//       );
+//       setFixtureFiltered(filtered);
+//     }
+//   }, [fixture, stadium]);
+
+//   return (
+//     <div>
+//       <div className="content my-2">
+//         <div className="container-fluid">
+//           <div className="row">
+//             <div className="col-12">
+//               <div className="card card-outline card-dark shadow">
+//                 <div className="card-header table-shadow">
+//                   <div className="row my-3">
+//                     <div className="col-12 text-center ">
+//                       <div
+//                         className="btn-group btn-group-toggle"
+//                         data-toggle="buttons"
+//                       >
+//                         <label className="btn btn-outline-primary active">
+//                           <input
+//                             type="radio"
+//                             name="allStadium"
+//                             id="allStadium"
+//                             autoComplete="off"
+//                             defaultChecked
+//                             value={0}
+//                             onClick={(event) => {
+//                               setStadium(event.target.value);
+//                             }}
+//                           />
+//                           All Stadium
+//                         </label>
+//                         <label className="btn btn-outline-primary">
+//                           <input
+//                             type="radio"
+//                             name="stadium1"
+//                             id="stadium1"
+//                             autoComplete="off"
+//                             value={1}
+//                             onClick={(event) => {
+//                               setStadium(event.target.value);
+//                             }}
+//                           />
+//                           Stadium 1
+//                         </label>
+//                         <label className="btn btn-outline-primary">
+//                           <input
+//                             type="radio"
+//                             name="stadium2"
+//                             id="stadium2"
+//                             autoComplete="off"
+//                             value={2}
+//                             onClick={(event) => {
+//                               setStadium(event.target.value);
+//                             }}
+//                           />
+//                           Stadium 2
+//                         </label>
+//                       </div>
+//                     </div>
+//                   </div>
+//                   <div className="row">
+//                     <div className="table-responsive mat-elevation-z2">
+//                       <table className="table table-hover table-striped text-center table-fixed ">
+//                         <thead className="thead-dark">
+//                           <tr>
+//                             <th width="15%">Home</th>
+//                             <th width="15%"></th>
+//                             <th width="15%">Away</th>
+//                             <th width="15%">Referee</th>
+//                           </tr>
+//                         </thead>
+//                         <tbody>
+//                           {fixtureFiltered.map((val) => {
+//                             return (
+//                               <tr key={val.id}>
+//                                 <td>{val.homeTeam}</td>
+//                                 <td>
+//                                   <ul>
+//                                     <li>{val.kickOffTime}</li>
+//                                     <li
+//                                       className="p-0"
+//                                       style={{
+//                                         color: "#000080",
+//                                       }}
+//                                     >
+//                                       StdNo. {val.stadiumNumber}
+//                                     </li>
+//                                     <li style={{ fontSize: "1.2rem" }}>
+//                                       {val.homeScores} - {val.awayScores}
+//                                     </li>
+//                                   </ul>
+//                                 </td>
+//                                 <td>{val.awayTeam}</td>
+//                                 <td>{val.refereeTeam}</td>
+//                               </tr>
+//                             );
+//                           })}
+//                         </tbody>
+//                       </table>
+//                     </div>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 const Fixture = () => {
-  _.sortBy(matches, ["kickOffTime", "stadiumNumber"]);
-  const [fixture, setFixture] = useState(matches);
-  const [fixtureFiltered, setFixtureFiltered] = useState(fixture);
-  const [stadium, setStadium] = useState(0);
-
+  const [Datamatch , setDatamatch] = useState([]);
   useEffect(() => {
-    if (+stadium === 0) {
-      setFixtureFiltered(fixture);
-    } else {
-      let filtered = fixture.filter(
-        (match) => match.stadiumNumber === +stadium
-      );
-      setFixtureFiltered(filtered);
-    }
-  }, [fixture, stadium]);
+    const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
+    const URL = "https://itreuionapi.herokuapp.com/match";
+    trackPromise(
+      axios({
+        method: "get",
+        url: PROXY_URL + URL,
+        data: {
+          KEY: "VALUE",
+        },
+      })
+        .then((res) => {
+          console.log(res.data.matchs)
+          setDatamatch(res.data.matchs);
+        })
+        .catch((err) => console.log(err))
+    );
+  }, []);
+  
+  const Match = (props) => {
+    return(
+      <tr>
+      <td>{props.match.HomeTeamId}</td>
+      <td>
+        <ul>
+            <li>{props.match.Kickoff}</li>
+            <li
+              className="p-0"
+              style={{
+                color: "#000080",
+              }}
+              >
+              StdNo. {props.match.StadiumId}
+            </li>
+            <li style={{ fontSize: "1.2rem" }}>
+            {props.match.HomeScores} - {props.match.AwayScores}
+            </li>
+        </ul>
+      </td>
+          <td>{props.match.AwayTeamId}</td>
+          <td>{props.match.RefereeTeamId}</td>
+      </tr>
+    )
+  }
 
-  return (
+  const Rendermatch = () => {
+    return Datamatch.map((match) => {
+      return <Match match={match} key={match.id}/>
+    })
+  }
+
+  return(
     <div>
       <div className="content my-2">
         <div className="container-fluid">
@@ -72,9 +235,9 @@ const Fixture = () => {
                             autoComplete="off"
                             defaultChecked
                             value={0}
-                            onClick={(event) => {
-                              setStadium(event.target.value);
-                            }}
+                            // onClick={(event) => {
+                            //   setStadium(event.target.value);
+                            // }}
                           />
                           All Stadium
                         </label>
@@ -85,9 +248,9 @@ const Fixture = () => {
                             id="stadium1"
                             autoComplete="off"
                             value={1}
-                            onClick={(event) => {
-                              setStadium(event.target.value);
-                            }}
+                            // onClick={(event) => {
+                            //   setStadium(event.target.value);
+                            // }}
                           />
                           Stadium 1
                         </label>
@@ -98,9 +261,9 @@ const Fixture = () => {
                             id="stadium2"
                             autoComplete="off"
                             value={2}
-                            onClick={(event) => {
-                              setStadium(event.target.value);
-                            }}
+                            // onClick={(event) => {
+                            //   setStadium(event.target.value);
+                            // }}
                           />
                           Stadium 2
                         </label>
@@ -119,7 +282,8 @@ const Fixture = () => {
                           </tr>
                         </thead>
                         <tbody>
-                          {fixtureFiltered.map((val) => {
+                          {Rendermatch()}
+                          {/* {fixtureFiltered.map((val) => {
                             return (
                               <tr key={val.id}>
                                 <td>{val.homeTeam}</td>
@@ -143,7 +307,7 @@ const Fixture = () => {
                                 <td>{val.refereeTeam}</td>
                               </tr>
                             );
-                          })}
+                          })} */}
                         </tbody>
                       </table>
                     </div>
@@ -155,7 +319,9 @@ const Fixture = () => {
         </div>
       </div>
     </div>
-  );
-};
+
+
+  )
+}
 
 export default Fixture;
